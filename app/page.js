b7430f016ce78382745817a6eee2e0f5b50ae505
OmPanchal <oapanchal@gmail.com>
2026-04-20 23:37:39 +0100

    Made use of new Components

--- app/page.js
+++ app/page.js
@@ -1,21 +1,28 @@
 import Image from "next/image";
-import logo from "../images/logo.png"
-
+import logo from "../images/logo.png";
+import Card from "@/components/Card";
+import Paragraph from "@/components/Paragraph";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-svh select-none">
-      <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center h-svh bg-transparent">
+      <div className="flex flex-col items-center justify-center bg-transparent">
         <div className="flex flex-row items-center justify-center">
-          <Image alt="logo" 
-                  src={logo}
-                  width={120}
-                  className="-mr-2"></Image>
-          <h1 className="text-7xl">
+          <Image
+            alt="logo"
+            src={logo}
+            width={160}
+            loading="eager"
+            className="-mr-2"
+          ></Image>
+          <Header className="text-8xl font-[WorkSansBold] text-dark">
             Tempo
-          </h1>
+          </Header>
         </div>
-        <p className="font-normal text-xs -mt-2.5">Be consistent!</p>
+        <Card>
+          <Paragraph>Be consistent!</Paragraph>
+        </Card>
       </div>
     </div>
   );
